13232bfca2b090dc7ee62d7ec5b0310cc260879e
nebula919 <benwalterscoding@gmail.com>
2019-09-09 13:07:42 +0000

    updated schema for students

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190908224900) do
+ActiveRecord::Schema.define(version: 20190909120130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,18 @@ ActiveRecord::Schema.define(version: 20190908224900) do
     t.integer  "user_id"
     t.decimal  "earnings"
     t.text     "class_notes"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "name"
+    t.integer  "age"
+    t.integer  "level"
+    t.string   "location"
+    t.date     "birthday"
+    t.text     "strength_weak"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
