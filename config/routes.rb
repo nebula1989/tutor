7acfa2255500ff6ee90d60782382d2e06f1dc0d2
nebula919 <benwalterscoding@gmail.com>
2019-10-13 13:57:46 +0000

    upgrade rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  
-  resources :students
-  resources :class_reports
-  get 'earnings/index', to: 'earnings#index', as: :earnings
-  get 'pages/dashboard', to: 'pages#dashboard', as: :dashboard
-  root 'pages#index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
