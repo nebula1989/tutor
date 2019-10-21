09587a7dd10d5893b0382594933101d468555c8a
nebula919 <benwalterscoding@gmail.com>
2019-10-21 10:34:04 +0000

    uniqueness student model but not for level

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ApplicationRecord
   belongs_to :user
-  validates :name, :level, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  validates :level, presence: true
   paginates_per 10
 end
